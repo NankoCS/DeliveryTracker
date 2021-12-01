05adf3f5dc079ac82e90b5102c41d4e7e601cd45
Raphael Blanchard <blanchard.raphael.pro@gmail.com>
2021-12-01 18:52:33 +0100

    Trying commits on git

--- Bicycle.java
+++ Bicycle.java
@@ -4,6 +4,7 @@ public class Bicycle extends Vehicule {
 
     public Bicycle(String name, double price, double avgSpeed){
         super(name, price, avgSpeed, 0.0, 0.0, price/Parameters.bicycleCostDividor);
+        //this line is a test to see how git reacts to changes
     }
 
 
