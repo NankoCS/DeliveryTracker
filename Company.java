1e8fdfb9a0b42c6d0fe9c6ed1f3517de40fc43ea
Raphael Blanchard <blanchard.raphael.pro@gmail.com>
2021-12-10 21:46:59 +0100

    DELETED THE AL TECHNIQUE IN COMPANY CLASS

--- Company.java
+++ Company.java
@@ -2,8 +2,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Company {
-    private ArrayList<Vehicule> listOfVehicules;
-    private ArrayList<Employee> listOfEmployees;
+
     private HashMap<String, ArrayList<Employee>> MapOfEmployees;
     private HashMap<String, ArrayList<Vehicule>> MapOfVehicules;
     private String name;
@@ -11,8 +10,6 @@ public class Company {
     private Coordinates setOfCoordinates;
 
     public Company(String name, String adress, Coordinates setOfCoordinates){
-        this.listOfEmployees = new ArrayList<Employee>();
-        this.listOfVehicules = new ArrayList<Vehicule>();
         this.MapOfEmployees = new HashMap<String, ArrayList<Employee>>();
         this.MapOfVehicules = new HashMap<String, ArrayList<Vehicule>>();
         this.name = name;
@@ -32,13 +29,6 @@ public class Company {
         return this.setOfCoordinates;
     }
 
-    public ArrayList<Employee> getListOfEmployees(){
-        return this.listOfEmployees;
-    }
-
-    public ArrayList<Vehicule> getListOfVehicules(){
-        return this.listOfVehicules;
-    }
 
     public HashMap<String, ArrayList<Vehicule>> getHashMap(){
         return MapOfVehicules;
@@ -49,32 +39,32 @@ public class Company {
     }
 
     public void addEmployee(Employee employee){
-        this.listOfEmployees.add(employee);
+        employee.addToCompany(this.MapOfEmployees);
     }
 
-    public ArrayList<Delivery> getAllDoableDeliveries(double weight, double duration){
-        Delivery tmp;
-        ArrayList<Delivery> listOfDeliveries = new ArrayList<Delivery>();
-        for (int i = 0; i < this.listOfEmployees.size(); i++){
-            for (int j = 0; j < this.listOfVehicules.size(); j++){
-                tmp = new Delivery(weight, duration, this.listOfEmployees.get(i), this.listOfVehicules.get(j));
-                if (tmp.validEmployeeAndVehicule()){
-                    listOfDeliveries.add(tmp);
-                }
-            }
-        }
-        return listOfDeliveries;
-    }
+    // public ArrayList<Delivery> getAllDoableDeliveries(double weight, double duration){
+    //     Delivery tmp;
+    //     ArrayList<Delivery> listOfDeliveries = new ArrayList<Delivery>();
+    //     for (int i = 0; i < this.listOfEmployees.size(); i++){
+    //         for (int j = 0; j < this.listOfVehicules.size(); j++){
+    //             tmp = new Delivery(weight, duration, this.listOfEmployees.get(i), this.listOfVehicules.get(j));
+    //             if (tmp.validEmployeeAndVehicule()){
+    //                 listOfDeliveries.add(tmp);
+    //             }
+    //         }
+    //     }
+    //     return listOfDeliveries;
+    // }
 
 
 
 
-    public String toString(){
-        return 
-        "\n--- " + this.getName() + " ---" + " \n" 
-        + "Location: " + this.getAdress() + " - " + this.getCoordinates().toString() + "\n"
-        + "Number of Employees: " + this.listOfEmployees.size() + "\n"
-        + "Number of vehicules: " + this.listOfVehicules.size();
-    }
+    // public String toString(){
+    //     return 
+    //     "\n--- " + this.getName() + " ---" + " \n" 
+    //     + "Location: " + this.getAdress() + " - " + this.getCoordinates().toString() + "\n"
+    //     + "Number of Employees: " + this.listOfEmployees.size() + "\n"
+    //     + "Number of vehicules: " + this.listOfVehicules.size();
+    // }
 
 }
